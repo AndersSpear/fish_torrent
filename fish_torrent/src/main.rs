d3480c735b071e62802a2ac7d4663cdb7f404e6f
Paul <python8u@gmail.com>
2023-12-06 03:11:48 -0500

    got bencoding.

--- fish_torrent/src/main.rs
+++ fish_torrent/src/main.rs
@@ -23,23 +23,10 @@ mod p2p;
 
 use std::io::prelude::*;
 use std::net::TcpStream;
+use urlencoding::encode;
+mod tracker;
 
 fn main() -> std::io::Result<()> {
-
-    let request_str = "GET / HTTP/1.1\r\nHost: 127.0.0.1:7878\r\n\r\n";
-    let request_bytes = request_str.as_bytes();
-
-    //replace with the TcpStream from alexandra
-    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
-    //let mut stream = TcpStream::connect("www.google.com:80")?;
-    stream.write_all(request_bytes)?;
-    stream.flush()?;
-
-    let mut response = Vec::new();
-    stream.read_to_end(&mut response)?;
-
-    let response_str = String::from_utf8_lossy(&response);
-    println!("{}", response_str);
+    tracker::send_tracker_request();
     Ok(())
 }
-
--- fish_torrent/src/tracker.rs
+++ fish_torrent/src/tracker.rs
@@ -3,7 +3,9 @@
 //import something to have access to the TCP stream
 
 
+use std::io::prelude::*;
 use std::net::TcpStream;
+use urlencoding::encode;
 
 // you just got a http response back from the tracker, process it!
 // call peers::update_peer_list() with the list of peers
@@ -23,35 +25,46 @@ fn handle_tracker_response(/*no arguments*/){
 // form req
     //specific format
 // send req
-fn send_tracker_request(tracker_url: &str){
+pub fn send_tracker_request() -> std::io::Result<()>{
+    // BitTorrent-specific parameters
+    let info_hash = "aaaaaaaaaaaaaaaaaaaa"; // needs to be 20 bytes
+    let peer_id = "aaaaaaaaaaaaaaaaaaaa";     // needs to be 20 bytes
+    let port = 6881; //port that we're running on
+    let uploaded = 0; //
+    let downloaded = 0;
+    let left = 0;
 
-    let request_str = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n";
+    // URL-encode the info_hash and peer_id
+    let encoded_info_hash = encode(info_hash);
+    let encoded_peer_id = encode(peer_id);
 
-    //replace with the TcpStream from alexandra
-    let mut stream = TcpStream::connect("www.google.com:80")?;
+    // Construct the tracker GET request URL
+    let tracker_url = format!(
+        "/announce?info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&compact=1",
+        encoded_info_hash, encoded_peer_id, port, uploaded, downloaded, left
+    );
+
+    // Construct the GET request string
+    let request_str = format!("GET {} HTTP/1.1\r\nHost: poole.cs.umd.edu\r\n\r\n", tracker_url);
+    //let request_str = "GET /announce HTTP/1.1\r\nHost: poole.cs.umd.edu\r\n\r\n";
+
+    // Convert the request string to bytes
+    let request_bytes = request_str.as_bytes();
+
+    // Connect to the tracker
+    let mut stream = TcpStream::connect("poole.cs.umd.edu:6969")?;
     stream.write_all(request_bytes)?;
+    stream.flush()?;
 
+    // Read the response from the tracker
     let mut response = Vec::new();
     stream.read_to_end(&mut response)?;
 
+    // Convert the response to a string and print it
     let response_str = String::from_utf8_lossy(&response);
     println!("{}", response_str);
 
-
-    //create request
-        // parameters
-            // info_hash
-            // peer_id
-            // port
-            // uploaded
-            // downloaded
-            // compact
-            // left
-        // URL
-    
-    
-
-
+    Ok(())
 }
 
 // send the initial request to the tracker. (not necessary?)
