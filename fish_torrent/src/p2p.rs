8cb010d2da63617bff17776edc309e5b227231b9
aspear <anders@aspear.net>
2023-12-05 22:20:51 -0500

    added some things in p2p, going to eep, ill fill these out in the mornign

--- fish_torrent/src/p2p.rs
+++ fish_torrent/src/p2p.rs
@@ -18,26 +18,40 @@ enum MessageType {
     Interested,
     NotInterested,
     Have {index: usize},
-    Bitfield {bitfield: BitVec}, // TODO: Temp type for bitfield
+    Bitfield {field: BitVec}, // BitVec is a bitvector from the bitvec crate
     Request {index: usize, begin: usize, length: usize},
-    Piece {index: usize, begin: usize, block: usize},
+    Piece {index: usize, begin: usize, block: Vec<u8>},
     Cancel {index: usize, begin: usize, length: usize},
     KeepAlive, // KeepAlive is last because it does not have an associated
               // id in the protocol. This way choke starts at id 0.
     //Port // DHT Tracker is not supported, so this msg is not handled.
     Undefined,
+    HandshakeResponse,
 }
 
-fn handle_message(sockfd: &u32){
+
+
+// called when socket triggers, either a handshake response or a message
+fn handle_message(sockfd: u32) -> Message<'static>{
     let peer: &Peer = peers::find_peer_by_sockfd(sockfd);
-    let msg = get_message(peer);
+    //let msg: Message<'a> = get_message(peer);
+
+    //read the message into a buffer
+    //see if its a new handshake, a handshakr response 
+
+    Message {
+        peer: peer,
+        m_type: MessageType::Undefined
+    }
 }
 
+// fn recv_message(sockfd: u32) -> Message<'a> {
+//     //read the message into a buffer
+//     //see if its a new handshake, a handshakr response 
+//     //if its a handshake response, return a handshake response message
+//     //if its a message, return a message
 
-fn get_message(peer: &Peer) -> Message {
-    let msg = Message{peer, m_type: MessageType::Undefined};
-    msg
-}
+// }
 
 // TODO: Another way to implement the above is an associated function/method
 impl Message<'_> {
@@ -52,5 +66,17 @@ impl Message<'_> {
     }
 }
 
-// TODO add the remaining functions!
+// if we get chcked, make sure to remove the send buffer for that person
 fn handle_choke(msg: &Message) {}
+
+//can handle sending any type of message
+//queues in some sort of send list
+fn send_message(msg: Message) {}
+
+// called right after we created a new peer
+// sends the initial handshake
+fn send_handshake(peer: &Peer) {
+    //get tcpstream
+    //create handshake message
+    //send the handshake message
+}
