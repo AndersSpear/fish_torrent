965f92d5d2621a2bce4aad2aeaae5f0bb460d754
vut <vut@terpmail.umd.edu>
2023-12-07 04:15:00 -0500

    Added some file writing stuff

--- fish_torrent/src/file.rs
+++ fish_torrent/src/file.rs
@@ -1,6 +1,6 @@
 // handle file reading and writing
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs::OpenOptions;
+use std::io::{Seek, SeekFrom, Write};
 
 use memmap2::MmapMut;
 
@@ -13,8 +13,20 @@ static mut SIZE: Option<usize> = None;
 
 type Piece = Vec<u8>;
 
-fn file_init(name: &str, piece_size: usize) {
-    let file = File::create(name).expect("awudh");
+fn init_file(name: &str, piece_size: usize) {
+    //let file = File::create(name).expect("awudh");
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(name)
+        .expect("wow");
+
+    //Space allocation
+    file.seek(SeekFrom::Start(piece_size.try_into().unwrap())).unwrap();
+    file.write_all(&[0]).unwrap();
+    file.seek(SeekFrom::Start(0)).unwrap();
+
     unsafe {
         FILE = Some(MmapMut::map_mut(&file).expect("wacky"));
         SIZE = Some(piece_size);
@@ -27,9 +39,21 @@ fn write_block(index: usize, begin: usize, block: Vec<u8>){
             if let Some(size) = SIZE {
                 file[((index * size) + begin)..block.len()].copy_from_slice(&block);
             }
+
+            file.flush().unwrap();
         }
     }
 }
 
 // we just recieved a request for a block, read it and send it out too?
 fn read_block(){}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn write_test_1() {
+        init_file("Hi", 10);
+        write_block(0, 0, Vec::from([b'a', b'b', b'c', b'd']));
+    }
+}
