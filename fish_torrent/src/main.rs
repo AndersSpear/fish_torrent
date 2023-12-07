53df581f2ea4c253d963424ea51e6e6197fbf783
alexia6119 <67676619+alexia6119@users.noreply.github.com>
2023-12-06 22:58:15 -0500

    added arg parsing

--- fish_torrent/src/main.rs
+++ fish_torrent/src/main.rs
@@ -3,15 +3,31 @@
 // handles epoll event loop
 // triggers peer tracker, p2p, strategy, on a timer
 
-mod p2p;
+// mod p2p;
 mod peers;
 mod torrent;
 
 use mio::{Events, Poll, Interest, Token};
-use mio::net::TcpStream
 use std::net::{self, SocketAddr};
+use clap::Parser;
+
+/// Takes in the port and torrent file
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Port to bind this client to
+    #[arg(short, long, default_value_t = 4170)]
+    port: u16,
+
+    /// Torrent file you wish to parse
+    #[arg(short, long)]
+    file: String,
+}
 
 fn main() {
+
+    // you'll never guess what this line does
+    let args = Args::parse();
 
     // set up the server addr
     // TODO: read in torrent file, ask tracker.rs to talk with tracker
