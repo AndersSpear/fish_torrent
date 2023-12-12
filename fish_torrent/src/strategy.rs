70d6e10a4cb4eee4d9f2e37c9c408a7cd092c1f3
aspear <anders@aspear.net>
2023-12-11 21:30:48 -0500

    added strategy api

--- fish_torrent/src/strategy.rs
+++ fish_torrent/src/strategy.rs
@@ -4,6 +4,13 @@
 use crate::peers::Peers;
 use crate::p2p::*;
 
+
+/// gives a list of messages that need to be sent out
 pub fn what_do(peers: &mut Peers) -> Vec<Messages>{
+    !unimplemented();
+}
+
+/// tell strategy what didnt get sent out, 
+pub fn these_didnt_happen(messages: Vec<Messages>){
     !unimplemented();
 }
